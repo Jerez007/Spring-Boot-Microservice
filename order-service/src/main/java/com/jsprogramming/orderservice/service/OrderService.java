841058942c65a1c8a37f33e7904495f8a654fd3a
JJ <jerez.solis@gmail.com>
2023-06-27 20:18:40 -0700

    updated OrderService to reference webClientBuilder

--- order-service/src/main/java/com/jsprogramming/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/jsprogramming/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -39,7 +39,7 @@ public class OrderService {
                 .toList();
 
         // Call Inventory Service and place order if product is in stock using WebClient
-        InventoryResponse[] inventoryResponseArray = webClient.get()
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
                 .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
