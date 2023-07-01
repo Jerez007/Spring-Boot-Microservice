a43d67825058d414fbffbe957d6e8f538996e42b
JJ <jerez.solis@gmail.com>
2023-07-01 12:47:49 -0700

    added security to eureka server

--- discovery-server/src/main/java/com/jsprogramming/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/jsprogramming/discoveryserver/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.jsprogramming.discoveryserver.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
